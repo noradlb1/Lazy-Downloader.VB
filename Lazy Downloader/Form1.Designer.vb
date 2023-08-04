779eca98e23a6d34b8267196c11928a737f176f0
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2023-08-04 04:08:21 +0200

    Lazy-Downloader.VB
    Lazy-Downloader.VB

---
+++ Lazy Downloader/Form1.Designer.vb
@@ -0,0 +1,337 @@
+﻿
+Imports System
+
+Namespace Lazy_Downloader
+    Partial Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <paramname="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+            groupBox1 = New Windows.Forms.GroupBox()
+            checkBox20 = New Windows.Forms.CheckBox()
+            checkBox19 = New Windows.Forms.CheckBox()
+            checkBox18 = New Windows.Forms.CheckBox()
+            checkBox17 = New Windows.Forms.CheckBox()
+            checkBox16 = New Windows.Forms.CheckBox()
+            checkBox15 = New Windows.Forms.CheckBox()
+            checkBox14 = New Windows.Forms.CheckBox()
+            checkBox13 = New Windows.Forms.CheckBox()
+            checkBox12 = New Windows.Forms.CheckBox()
+            checkBox11 = New Windows.Forms.CheckBox()
+            checkBox10 = New Windows.Forms.CheckBox()
+            checkBox9 = New Windows.Forms.CheckBox()
+            checkBox8 = New Windows.Forms.CheckBox()
+            checkBox7 = New Windows.Forms.CheckBox()
+            checkBox6 = New Windows.Forms.CheckBox()
+            checkBox5 = New Windows.Forms.CheckBox()
+            checkBox4 = New Windows.Forms.CheckBox()
+            checkBox3 = New Windows.Forms.CheckBox()
+            checkBox2 = New Windows.Forms.CheckBox()
+            checkBox1 = New Windows.Forms.CheckBox()
+            pictureBox1 = New Windows.Forms.PictureBox()
+            groupBox1.SuspendLayout()
+            CType(pictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+            SuspendLayout()
+            ' 
+            ' groupBox1
+            ' 
+            groupBox1.Controls.Add(checkBox20)
+            groupBox1.Controls.Add(checkBox19)
+            groupBox1.Controls.Add(checkBox18)
+            groupBox1.Controls.Add(checkBox17)
+            groupBox1.Controls.Add(checkBox16)
+            groupBox1.Controls.Add(checkBox15)
+            groupBox1.Controls.Add(checkBox14)
+            groupBox1.Controls.Add(checkBox13)
+            groupBox1.Controls.Add(checkBox12)
+            groupBox1.Controls.Add(checkBox11)
+            groupBox1.Controls.Add(checkBox10)
+            groupBox1.Controls.Add(checkBox9)
+            groupBox1.Controls.Add(checkBox8)
+            groupBox1.Controls.Add(checkBox7)
+            groupBox1.Controls.Add(checkBox6)
+            groupBox1.Controls.Add(checkBox5)
+            groupBox1.Controls.Add(checkBox4)
+            groupBox1.Controls.Add(checkBox3)
+            groupBox1.Controls.Add(checkBox2)
+            groupBox1.Controls.Add(checkBox1)
+            groupBox1.Location = New Drawing.Point(12, 6)
+            groupBox1.Name = "groupBox1"
+            groupBox1.Size = New Drawing.Size(412, 158)
+            groupBox1.TabIndex = 0
+            groupBox1.TabStop = False
+            ' 
+            ' checkBox20
+            ' 
+            checkBox20.AutoSize = True
+            checkBox20.Location = New Drawing.Point(302, 111)
+            checkBox20.Name = "checkBox20"
+            checkBox20.Size = New Drawing.Size(102, 17)
+            checkBox20.TabIndex = 19
+            checkBox20.Text = "Process Hacker"
+            checkBox20.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox19
+            ' 
+            checkBox19.AutoSize = True
+            checkBox19.Location = New Drawing.Point(302, 88)
+            checkBox19.Name = "checkBox19"
+            checkBox19.Size = New Drawing.Size(77, 17)
+            checkBox19.TabIndex = 18
+            checkBox19.Text = "qBittorrent "
+            checkBox19.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox18
+            ' 
+            checkBox18.AutoSize = True
+            checkBox18.Location = New Drawing.Point(302, 65)
+            checkBox18.Name = "checkBox18"
+            checkBox18.Size = New Drawing.Size(99, 17)
+            checkBox18.TabIndex = 17
+            checkBox18.Text = "Sandboxie Plus"
+            checkBox18.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox17
+            ' 
+            checkBox17.AutoSize = True
+            checkBox17.Location = New Drawing.Point(111, 111)
+            checkBox17.Name = "checkBox17"
+            checkBox17.Size = New Drawing.Size(57, 17)
+            checkBox17.TabIndex = 16
+            checkBox17.Text = "Firefox"
+            checkBox17.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox16
+            ' 
+            checkBox16.AutoSize = True
+            checkBox16.Location = New Drawing.Point(111, 88)
+            checkBox16.Name = "checkBox16"
+            checkBox16.Size = New Drawing.Size(68, 17)
+            checkBox16.TabIndex = 15
+            checkBox16.Text = "OperaGx"
+            checkBox16.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox15
+            ' 
+            checkBox15.AutoSize = True
+            checkBox15.Location = New Drawing.Point(111, 65)
+            checkBox15.Name = "checkBox15"
+            checkBox15.Size = New Drawing.Size(62, 17)
+            checkBox15.TabIndex = 14
+            checkBox15.Text = "Chrome"
+            checkBox15.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox14
+            ' 
+            checkBox14.AutoSize = True
+            checkBox14.Location = New Drawing.Point(302, 42)
+            checkBox14.Name = "checkBox14"
+            checkBox14.Size = New Drawing.Size(102, 17)
+            checkBox14.TabIndex = 13
+            checkBox14.Text = "Ubisoft Connect"
+            checkBox14.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox13
+            ' 
+            checkBox13.AutoSize = True
+            checkBox13.Location = New Drawing.Point(302, 20)
+            checkBox13.Name = "checkBox13"
+            checkBox13.Size = New Drawing.Size(78, 17)
+            checkBox13.TabIndex = 12
+            checkBox13.Text = "Battle.NET"
+            checkBox13.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox12
+            ' 
+            checkBox12.AutoSize = True
+            checkBox12.Location = New Drawing.Point(185, 111)
+            checkBox12.Name = "checkBox12"
+            checkBox12.Size = New Drawing.Size(111, 17)
+            checkBox12.TabIndex = 11
+            checkBox12.Text = "Epic Games Store"
+            checkBox12.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox11
+            ' 
+            checkBox11.AutoSize = True
+            checkBox11.Location = New Drawing.Point(185, 89)
+            checkBox11.Name = "checkBox11"
+            checkBox11.Size = New Drawing.Size(53, 17)
+            checkBox11.TabIndex = 10
+            checkBox11.Text = "Origin"
+            checkBox11.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox10
+            ' 
+            checkBox10.AutoSize = True
+            checkBox10.Location = New Drawing.Point(185, 66)
+            checkBox10.Name = "checkBox10"
+            checkBox10.Size = New Drawing.Size(56, 17)
+            checkBox10.TabIndex = 9
+            checkBox10.Text = "Steam"
+            checkBox10.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox9
+            ' 
+            checkBox9.AutoSize = True
+            checkBox9.Location = New Drawing.Point(185, 43)
+            checkBox9.Name = "checkBox9"
+            checkBox9.Size = New Drawing.Size(68, 17)
+            checkBox9.TabIndex = 8
+            checkBox9.Text = "Ccleaner"
+            checkBox9.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox8
+            ' 
+            checkBox8.AutoSize = True
+            checkBox8.Location = New Drawing.Point(185, 20)
+            checkBox8.Name = "checkBox8"
+            checkBox8.Size = New Drawing.Size(92, 17)
+            checkBox8.TabIndex = 7
+            checkBox8.Text = "BC Uninstaller"
+            checkBox8.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox7
+            ' 
+            checkBox7.AutoSize = True
+            checkBox7.Location = New Drawing.Point(111, 43)
+            checkBox7.Name = "checkBox7"
+            checkBox7.Size = New Drawing.Size(54, 17)
+            checkBox7.TabIndex = 6
+            checkBox7.Text = "Brave"
+            checkBox7.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox6
+            ' 
+            checkBox6.AutoSize = True
+            checkBox6.Location = New Drawing.Point(111, 20)
+            checkBox6.Name = "checkBox6"
+            checkBox6.Size = New Drawing.Size(70, 17)
+            checkBox6.TabIndex = 5
+            checkBox6.Text = "Telegram"
+            checkBox6.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox5
+            ' 
+            checkBox5.AutoSize = True
+            checkBox5.Location = New Drawing.Point(6, 111)
+            checkBox5.Name = "checkBox5"
+            checkBox5.Size = New Drawing.Size(87, 17)
+            checkBox5.TabIndex = 4
+            checkBox5.Text = "Visual Studio"
+            checkBox5.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox4
+            ' 
+            checkBox4.AutoSize = True
+            checkBox4.Location = New Drawing.Point(6, 88)
+            checkBox4.Name = "checkBox4"
+            checkBox4.Size = New Drawing.Size(99, 17)
+            checkBox4.TabIndex = 3
+            checkBox4.Text = "Notepad++ x64"
+            checkBox4.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox3
+            ' 
+            checkBox3.AutoSize = True
+            checkBox3.Location = New Drawing.Point(6, 65)
+            checkBox3.Name = "checkBox3"
+            checkBox3.Size = New Drawing.Size(88, 17)
+            checkBox3.TabIndex = 2
+            checkBox3.Text = "WinRAR x64"
+            checkBox3.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox2
+            ' 
+            checkBox2.AutoSize = True
+            checkBox2.Location = New Drawing.Point(6, 42)
+            checkBox2.Name = "checkBox2"
+            checkBox2.Size = New Drawing.Size(46, 17)
+            checkBox2.TabIndex = 1
+            checkBox2.Text = "VLC"
+            checkBox2.UseVisualStyleBackColor = True
+            ' 
+            ' checkBox1
+            ' 
+            checkBox1.AutoSize = True
+            checkBox1.Location = New Drawing.Point(6, 19)
+            checkBox1.Name = "checkBox1"
+            checkBox1.Size = New Drawing.Size(62, 17)
+            checkBox1.TabIndex = 0
+            checkBox1.Text = "Discord"
+            checkBox1.UseVisualStyleBackColor = True
+            ' 
+            ' pictureBox1
+            ' 
+            pictureBox1.Image = CType(resources.GetObject("pictureBox1.Image"), Drawing.Image)
+            pictureBox1.Location = New Drawing.Point(430, 6)
+            pictureBox1.Name = "pictureBox1"
+            pictureBox1.Size = New Drawing.Size(239, 158)
+            pictureBox1.SizeMode = Windows.Forms.PictureBoxSizeMode.StretchImage
+            pictureBox1.TabIndex = 1
+            pictureBox1.TabStop = False
+            AddHandler pictureBox1.Click, New EventHandler(AddressOf pictureBox1_Click)
+            ' 
+            ' Form1
+            ' 
+            AutoScaleDimensions = New Drawing.SizeF(6.0F, 13.0F)
+            AutoScaleMode = Windows.Forms.AutoScaleMode.Font
+            BackColor = Drawing.Color.LightSkyBlue
+            ClientSize = New Drawing.Size(677, 171)
+            Controls.Add(pictureBox1)
+            Controls.Add(groupBox1)
+            Name = "Form1"
+            Text = "Lazy Downloader"
+            groupBox1.ResumeLayout(False)
+            groupBox1.PerformLayout()
+            CType(pictureBox1, ComponentModel.ISupportInitialize).EndInit()
+            ResumeLayout(False)
+
+        End Sub
+
+#End Region
+
+        Private groupBox1 As Windows.Forms.GroupBox
+        Private checkBox18 As Windows.Forms.CheckBox
+        Private checkBox17 As Windows.Forms.CheckBox
+        Private checkBox16 As Windows.Forms.CheckBox
+        Private checkBox15 As Windows.Forms.CheckBox
+        Private checkBox14 As Windows.Forms.CheckBox
+        Private checkBox13 As Windows.Forms.CheckBox
+        Private checkBox12 As Windows.Forms.CheckBox
+        Private checkBox11 As Windows.Forms.CheckBox
+        Private checkBox10 As Windows.Forms.CheckBox
+        Private checkBox9 As Windows.Forms.CheckBox
+        Private checkBox8 As Windows.Forms.CheckBox
+        Private checkBox7 As Windows.Forms.CheckBox
+        Private checkBox6 As Windows.Forms.CheckBox
+        Private checkBox5 As Windows.Forms.CheckBox
+        Private checkBox4 As Windows.Forms.CheckBox
+        Private checkBox3 As Windows.Forms.CheckBox
+        Private checkBox2 As Windows.Forms.CheckBox
+        Private checkBox1 As Windows.Forms.CheckBox
+        Private pictureBox1 As Windows.Forms.PictureBox
+        Private checkBox20 As Windows.Forms.CheckBox
+        Private checkBox19 As Windows.Forms.CheckBox
+    End Class
+End Namespace
---
+++ Lazy Downloader/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Net
+Imports System.Windows.Forms
+
+Namespace Lazy_Downloader
+    Public Partial Class Form1
+        Inherits Form
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub pictureBox1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim path = Environment.CurrentDirectory
+            Dim dl = New WebClient()
+            If checkBox1.Checked Then dl.DownloadFile("https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86", "DiscordSetup.exe")
+            If checkBox2.Checked Then dl.DownloadFile("https://get.videolan.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe", "vlc-3.0.16-win64.exe")
+            If checkBox3.Checked Then dl.DownloadFile("https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-602fr.exe", "winrar-x64-602fr.exe")
+            If checkBox4.Checked Then dl.DownloadFile("https://notepad-plus-plus.org/repository/7.x/7.0/npp.7.Installer.x64.exe", "npp.7.Installer.x64.exe")
+            If checkBox5.Checked Then dl.DownloadFile("https://download.visualstudio.microsoft.com/download/pr/9dc321fd-8a9b-47ef-98a9-af0515e08d6f/533c91be0b61f481cb93758a3c6af09cef719cedbaa6e9916232918649fd4a35/vs_Community.exe", "vs_Community.exe")
+            If checkBox6.Checked Then dl.DownloadFile("https://telegram.org/dl/desktop/win64", "TelegramSetup.exe")
+            If checkBox7.Checked Then dl.DownloadFile("https://laptop-updates.brave.com/latest/winx64", "BraveSetup.exe")
+            If checkBox8.Checked Then dl.DownloadFile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BE060CBB4-0849-8C90-D483-C40EF182C24A%7D%26lang%3Dfr%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe", "ChromeSetup.exe")
+            If checkBox9.Checked Then dl.DownloadFile("https://net.geo.opera.com/opera_gx/stable/windows?utm_tryagain=yes&utm_source=google_via_opera_com&utm_medium=ose&utm_campaign=(none)_via_opera_com_https&http_referrer=https%3A%2F%2Fwww.google.com%2F&utm_site=opera_com&&utm_lastpage=opera.com/computer/thanks&dl_token=27647740", "OperaGxSetup.exe")
+            If checkBox10.Checked Then dl.DownloadFile("https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/fr/win/0eec0eb5d30526efbad3e6b369b201812546d07980318d7feef46149be820284/Firefox%20Installer.exe", "FirefoxSetup.exe")
+            If checkBox11.Checked Then dl.DownloadFile("https://dotsrc.dl.osdn.net/osdn/bulk-crap-uninstaller/75105/BCUninstaller_5.0_setup.exe", "BCUninstaller_5.0_setup.exe")
+            If checkBox12.Checked Then dl.DownloadFile("https://download.ccleaner.com/ccsetup582.exe", "ccsetup582.exe")
+            If checkBox13.Checked Then dl.DownloadFile("https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe", "SteamSetup.exe")
+            If checkBox14.Checked Then dl.DownloadFile("https://www.dm.origin.com/download", "OriginSetup.exe")
+            If checkBox15.Checked Then dl.DownloadFile("https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi", "EpicGamesLauncherInstaller.msi")
+            If checkBox16.Checked Then dl.DownloadFile("https://eu.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe&id=undefined", "Battle.net-Setup.exe")
+            If checkBox17.Checked Then dl.DownloadFile("https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UbisoftConnectInstaller.exe", "UbisoftConnectInstaller.exe")
+            If checkBox18.Checked Then dl.DownloadFile("https://github.com/sandboxie-plus/Sandboxie/releases/download/0.8.6/Sandboxie-Plus-x64-v0.8.6.exe", "Sandboxie-Plus-x64-v0.8.6.exe")
+            If checkBox19.Checked Then dl.DownloadFile("https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.3.6/qbittorrent_4.3.6_x64_setup.exe/download", "qbittorrent_4.3.6_x64_setup.exe")
+            If checkBox20.Checked Then dl.DownloadFile("https://sourceforge.net/projects/processhacker/files/processhacker2/processhacker-2.39-setup.exe/download", "processhacker-2.39-setup.exe")
+
+            If File.Exists(path & "\DiscordSetup.exe") Then Process.Start(path & "\DiscordSetup.exe")
+
+            If File.Exists(path & "\vlc-3.0.16-win64.exe") Then Process.Start(path & "\vlc-3.0.16-win64.exe")
+
+            If File.Exists(path & "\winrar-x64-602fr.exe") Then Process.Start(path & "\winrar-x64-602fr.exe")
+
+            If File.Exists(path & "\npp.7.Installer.x64.exe") Then Process.Start(path & "\npp.7.Installer.x64.exe")
+
+            If File.Exists(path & "\vs_Community.exe") Then Process.Start(path & "\vs_Community.exe")
+
+            If File.Exists(path & "\TelegramSetup.exe") Then Process.Start(path & "\TelegramSetup.exe")
+
+            If File.Exists(path & "\BraveSetup.exe") Then Process.Start(path & "\BraveSetup.exe")
+
+            If File.Exists(path & "\ChromeSetup.exe") Then Process.Start(path & "\ChromeSetup.exe")
+
+            If File.Exists(path & "\OperaGxSetup.exe") Then Process.Start(path & "\OperaGxSetup.exe")
+
+            If File.Exists(path & "\FirefoxSetup.exe") Then Process.Start(path & "\FirefoxSetup.exe")
+
+            If File.Exists(path & "\BCUninstaller_5.0_setup.exe") Then Process.Start(path & "\BCUninstaller_5.0_setup.exe")
+
+            If File.Exists(path & "\ccsetup582.exe") Then Process.Start(path & "\ccsetup582.exe")
+
+            If File.Exists(path & "\SteamSetup.exe") Then Process.Start(path & "\SteamSetup.exe")
+
+            If File.Exists(path & "\OriginSetup.exe") Then Process.Start(path & "\OriginSetup.exe")
+
+            If File.Exists(path & "\EpicGamesLauncherInstaller.msi") Then Process.Start(path & "\EpicGamesLauncherInstaller.msi")
+
+            If File.Exists(path & "\Battle.net-Setup.exe") Then Process.Start(path & "\Battle.net-Setup.exe")
+
+            If File.Exists(path & "\UbisoftConnectInstaller.exe") Then Process.Start(path & "\UbisoftConnectInstaller.exe")
+
+            If File.Exists(path & "\Sandboxie-Plus-x64-v0.8.6.exe") Then Process.Start(path & "\Sandboxie-Plus-x64-v0.8.6.exe")
+
+            If File.Exists(path & "\qbittorrent_4.3.6_x64_setup.exe") Then Process.Start(path & "\qbittorrent_4.3.6_x64_setup.exe")
+
+            If File.Exists(path & "\processhacker-2.39-setup.exe") Then Process.Start(path & "\processhacker-2.39-setup.exe")
+
+        End Sub
+    End Class
+End Namespace
